624a9e9d4f4797a359c71bdfd2f075e34751780e
Alejandro Ruete <alejandro@greensway.se>
2020-04-28 15:48:21 +0200

    progress towards the wraper

--- R/occurrence_details.R
+++ R/occurrence_details.R
@@ -5,16 +5,16 @@
 #' @references Associated NBN web service: \url{https://api.nbnatlas.org/#ws102}
 #' 
 #' @param uuid string: one or more record ids, as returned by \code{\link{occurrences}} (in the \code{data$id} column)
-#' @param verbose logical: show additional progress information? [default is set by nbn_config()]
+#' @param verbose logical: show additional progress information? [default is set by sbdi_config()]
 #' @return A named list (named by uuid), each element of which is a list containing the details for that uuid. This inner list will be empty if no match is found for the supplied uuid
-#' @seealso \code{\link{occurrences}} \code{\link{nbn_config}}
+#' @seealso \code{\link{occurrences}} \code{\link{sbdi_config}}
 #' @examples
 #' \dontrun{
 #' s1 <- occurrence_details("4fce3968-c4b0-4083-8896-78d8f213d517")
 #' str(s1,max.level=3)
 #' }
 #' @export occurrence_details
-occurrence_details <- function(uuid, verbose = nbn_config()$verbose) {
+occurrence_details <- function(uuid, verbose = sbdi_config()$verbose) {
   
   ALA4R::occurrence_details(uuid, verbose)
   
