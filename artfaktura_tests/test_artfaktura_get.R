c0fb0759ab0b0b13392772c1f2f53624371a0680
rukayaj@gmail.com <rukayaj@gmail.com>
2020-04-28 16:24:40 +0200

    Refactor a bit and start adding tests

---
+++ artfaktura_tests/test_artfaktura_get.R
@@ -0,0 +1,30 @@
+source("../R/artfaktura_get.R", chdir = TRUE)
+library(testthat)
+
+test_that("basic get functionality works", {
+  url <- 'https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/search?searchString=Ramaria%20primulina'
+  ramaria <- getArtfaktaResults(url)
+  expected <- list(6039941, '', 'Ramaria primulina')
+  names(expected) <- c('taxonId', 'swedishName', 'scientificName')
+  expected <- list(expected)
+  expect_equal(ramaria, expected)
+})
+
+test_that("get functionality throws error if invalid resource", {
+})
+
+test_that("get functionality throws error if key invalid or nonexistent", {
+})
+
+test_that('url is constructed correctly with a query', {
+})
+
+test_that('url is constructed correctly without a query', {
+})
+
+test_that("dataframe conversion works", {
+  taxonId = c(6039941)
+  swedishName = c('')
+  scientificName = c('Ramaria primulina')
+  expected = data.frame(taxonId, swedishName, scientificName)
+})
