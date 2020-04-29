c05e28241e713afb317acffbeee814b576cc3c96
rukayaj@gmail.com <rukayaj@gmail.com>
2020-04-29 17:20:08 +0200

    Forgot to commit last stuff

--- R/artfaktura_get.R
+++ R/artfaktura_get.R
@@ -4,43 +4,74 @@ library(keyring)
 ## https://api-portal.artdatabanken.se/
 # Reference https://api-portal.artdatabanken.se/docs/services/sandbox-species-observations---read/operations/59fb25717ec10608d0167c91?
 
-getArtfakturaResources <- function() {
-  resourceNames <- c("assessments", "biotopes", "landscapetypes", "natureconservation", "periods", "redlist", "search", "substrates", "taxonomy", "texts")
-  return(resourceNames)
+GetSpeciesData <- function(taxon_ids) { 
+  return(GetArtfaktaResults(ConstructQueryURL('', queryTerm='taxa', queryValue=taxon_ids))) 
+}
+GetAssessments <- function(taxon_ids) { 
+  return(GetArtfaktaResults(ConstructQueryURL('assessments', queryTerm='taxa', queryValue=taxon_ids))) 
+}
+GetBiotopes <- function(taxon_ids) { 
+  return(GetArtfaktaResults(ConstructQueryURL('biotopes', queryTerm='taxa', queryValue=taxon_ids))) 
+}
+GetLandscapeTypes <- function(taxon_ids) { 
+  return(GetArtfaktaResults(ConstructQueryURL('landscapetypes', queryTerm='taxa', queryValue=taxon_ids))) 
+}
+GetNatureConservation <- function(taxon_ids) { 
+  return(GetArtfaktaResults(ConstructQueryURL('natureconservation', queryTerm='taxa', queryValue=taxon_ids))) 
+}
+GetPeriods <- function() { 
+  return(GetArtfaktaResults(ConstructQueryURL('periods'))) 
+}
+GetPeriodsCurrent <- function() { 
+  return(GetArtfaktaResults(ConstructQueryURL('periods/current'))) 
+}
+GetRedlist <- function(taxon_ids, period_index=FALSE) {
+  if(period_index) {
+    url <- ConstructQueryURL('redlist', queryTerm='taxa', queryValue=taxon_ids, secondQueryTerm='period', secondQueryValue=period_index)
+  } else {
+    url <- ConstructQueryURL('redlist', queryTerm='taxa', queryValue=taxon_ids)
+  }
+  return(GetArtfaktaResults(url))
+}
+GetSearch <- function(taxon_ids_or_free_text) {
+  return(GetArtfaktaResults(ConstructQueryURL('search', queryTerm='searchString', queryValue=taxon_ids_or_free_text)))
+}
+GetSubstrates <- function(taxon_ids) { 
+  return(GetArtfaktaResults(ConstructQueryURL('substrates', queryTerm='taxa', queryValue=taxon_ids))) 
+}
+GetTaxonomy <- function(taxon_ids) { 
+  return(GetArtfaktaResults(ConstructQueryURL('taxonomy', queryTerm='taxa', queryValue=taxon_ids))) 
+}
+GetTexts <- function(taxon_ids) { 
+  return(GetArtfaktaResults(ConstructQueryURL('texts', queryTerm='taxa', queryValue=taxon_ids))) 
 }
 
-constructQueryURL <- function(resource, queryTerm='', queryValue='') {
+ConstructQueryURL <- function(resource, queryTerm='', queryValue='', secondQueryTerm='', secondQueryValue='') {
   baseURL <- "https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/"
   url <- paste0(baseURL, resource)
   if(queryTerm != '') {
-    url <- paste0(url, '?', queryTerm, '=', queryValue)
+    url <- paste0(url, '?', queryTerm, '=', URLencode(queryValue))
+  }
+  if(secondQueryTerm != '') {
+    url <- paste0(url, '&', secondQueryTerm, '=', URLencode(secondQueryValue))
   }
   return(url)
 }
 
-getArtfaktaResults <- function(url) {
+GetArtfaktaResults <- function(url) {
   key <- key_get(service="ArtdataAPI", username="r.s.johaadien@nhm.uio.no")
-  return(content(GET(url=url, add_headers("Ocp-Apim-Subscription-Key"=key)), as="parsed"))
+  get <- GET(url=url, add_headers("Ocp-Apim-Subscription-Key"=key))
+  return(.ConvertResultsToDataFrame(content(get, as="parsed")))
 }
 
-convertResultsToDataFrame <- function(list) {
-  data <- list
+.ConvertResultsToDataFrame <- function(data) {
   names <- names(unlist(data[[1]]))
-  DataMatrix <- matrix(unlist(data), ncol=length(names), byrow=T, dimnames = list(1:length(data), names))
-  DF <- data.frame(DataMatrix, stringsAsFactors=FALSE)
+  DataMatrix <- matrix(unlist(data), ncol = length(names), byrow = T, dimnames = list(1:length(data), names))
+  DF <- data.frame(DataMatrix, stringsAsFactors = FALSE)
   return(DF)
 }
 
 # Examples
-ramaria <- GETartfakta(resource = "search", query = "?searchString=Ramaria", key=key)
-taxaRamaria <- fakta2DF(ramaria)
+ramaria <- GetSearch(taxon_ids_or_free_text = 'Ramaria')
 
-substRamaria<-GETartfakta(resource = "substrates", query=paste0("?taxa=",
-                                                                paste0(taxaRamaria$taxonId, collapse = ",")),
-                          key=key)
-
-substRamaria[[1]]$speciesData
-subst<-lapply(substRamaria, function(x)x$speciesData)
-lengths(substRamaria)
-lengths(subst)
 
--- artfaktura_tests/test_artfaktura_get.R
+++ artfaktura_tests/test_artfaktura_get.R
@@ -2,35 +2,33 @@ source("../R/artfaktura_get.R", chdir = TRUE)
 library(testthat)
 
 test_that("basic get functionality works", {
-  url <- 'https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/search?searchString=Ramaria%20primulina'
-  ramaria <- getArtfaktaResults(url)
-  expected <- list(6039941, '', 'Ramaria primulina')
+  ramaria <- GetArtfaktaResults('https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/search?searchString=Ramaria%20primulina')
+  expected <- list('6039941', '', 'Ramaria primulina')
   names(expected) <- c('taxonId', 'swedishName', 'scientificName')
-  expected <- list(expected)
-  expect_equal(ramaria, expected)
+  expect_equal(str(ramaria), str(data.frame(expected)))
 })
 
-test_that("get functionality throws error if invalid resource", {
+test_that("get functionality can returns 0 records", {
 })
 
 test_that("get functionality throws error if key invalid or nonexistent", {
 })
 
 test_that('url is constructed correctly with a query', {
-  url = constructQueryURL('search', queryTerm='searchString', queryValue='Ramaria')
+  url = ConstructQueryURL('search', queryTerm='searchString', queryValue='Ramaria')
   expect_equal(url, 'https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/search?searchString=Ramaria')
 })
 
 test_that('url is constructed correctly with spaces in query', {
-  url = constructQueryURL('search', queryTerm='searchString', queryValue='Ramaria primulina')
+  url = ConstructQueryURL('search', queryTerm='searchString', queryValue='Ramaria primulina')
+  expect_equal(url, 'https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/search?searchString=Ramaria%20primulina')
 })
 
 test_that('url is constructed correctly without a query', {
+  expect_equal(ConstructQueryURL('search'), 'https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/search')
 })
 
-test_that("dataframe conversion works", {
-  taxonId = c(6039941)
-  swedishName = c('')
-  scientificName = c('Ramaria primulina')
-  expected = data.frame(taxonId, swedishName, scientificName)
+test_that('url is constructed correctly with two queries', {
+  url = ConstructQueryURL(resource='redlist', queryTerm='taxa', queryValue='1001270', secondQueryTerm='period', secondQueryValue='100')
+  expect_equal(url, 'https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/redlist?taxa=1001270&period=100')
 })
